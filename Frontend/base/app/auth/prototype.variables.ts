42636d40c1c3dde2a44c7acc9cdb964729de52f7
vsukumaran vsukumaran <vsukumaran@vanenburg.com>
2024-06-24 13:58:20 +0000

    Commit for the generated/updated files via system

---
+++ Frontend/base/app/auth/prototype.variables.ts
@@ -0,0 +1,5 @@
+export class PrototypeVariables {
+	public static APP_ID = "595d7a62-a50c-4969-ad75-7dc36c5ed9f4";
+	public static TENANT_ID = "99dc49fd-c8d1-4356-9b81-f896135ab021";
+	public static DESIGN_STUDIO_URL = "https://eva-design-studio-vsukumaran-designstudio-lm6qdjq2fa-ez.a.run.app/";
+}
